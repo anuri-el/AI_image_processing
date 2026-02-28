cd7280fadb4c38dfd7d13546d0178bc24de21940
anuri-el <sofia.poteychuk@gmail.com>
2026-02-28 13:52:29 +0200

    add object tracking to video processing

--- detection/fruits_detection_yolo.py
+++ detection/fruits_detection_yolo.py
@@ -4,15 +4,20 @@ from ultralytics import YOLO
 
 
 def main():
-    # img_path = "input/fruits/pexels-rachel-claire-5864750.jpg"
-    img_path = "input/fruits/pexels-viktoria-slowikowska-5677917.jpg"
-    vid_path = "input/fruits/8203772-uhd_3840_2160_24fps.mp4"
+    img_path = "input/fruits/pexels-rachel-claire-5864750.jpg"
+    # img_path = "input/fruits/pexels-viktoria-slowikowska-5677917.jpg"
 
-    annotated_img = detect_obj(img_path)
-    annotated_img = resize_frame(annotated_img, scale=0.3)
+    # vid_path = "input/fruits/8203772-uhd_3840_2160_24fps.mp4"
+    # vid_path = "input/fruits/15645011-hd_1920_1080_25fps.mp4"
+    vid_path = "input/fruits/14249403_1920_1080_25fps.mp4"
+
+    annotated_img, counts = detect_obj(img_path, conf=0.5, scale=0.3)
     cv.imshow("detected objects", annotated_img)
+    # for label, count in counts.items():
+    #     print(f"{label} : {count}")
 
-    detect_obj_video(vid_path)
+    detect_obj_video(vid_path, conf=0.3, scale=0.3)
+    track_objects(vid_path, conf=0.3, scale=0.3)
 
     cv.waitKey(0)
     cv.destroyAllWindows()
@@ -25,23 +30,29 @@ def resize_frame(img, scale=0.5):
     return img
 
 
-# to-do: only prob >0.6
-def detect_obj(source):
+def detect_obj(source, conf=0.6, scale=1):
     model = YOLO("models/yolov8m.pt")
-    results = model(source)
+    results = model(source, conf=conf)
 
     annotated_frame = results[0].plot()
+    annotated_frame = resize_frame(annotated_frame, scale=scale)
+
+    counts = {}
+    for box in results[0].boxes:
+        cls_id = int(box.cls[0])
+        label = model.names[cls_id]
+        counts[label] = counts.get(label, 0) + 1
 
     if isinstance(source, str):
         basename = os.path.splitext(os.path.basename(source))[0]
         # output_path = f"output/{source[6:-4]}_out.jpg"
-        output_path = f"output/{basename}_out.jpg"
+        output_path = f"output/{basename}_{conf}_out.jpg"
         cv.imwrite(output_path, annotated_frame)
 
-    return annotated_frame
+    return annotated_frame, counts
 
 
-def detect_obj_video(video_path):
+def detect_obj_video(video_path, conf=0.6, scale=1):
     capture = cv.VideoCapture(video_path)
 
     width = int(capture.get(cv.CAP_PROP_FRAME_WIDTH))
@@ -49,25 +60,60 @@ def detect_obj_video(video_path):
     fps = capture.get(cv.CAP_PROP_FPS)
 
     basename = os.path.splitext(os.path.basename(video_path))[0]
-    output_path = f"output/{basename}_out.mp4"
+    output_path = f"output/{basename}_{conf}_frame_out.mp4"
     fourcc = cv.VideoWriter_fourcc(*"mp4v")
 
     out = cv.VideoWriter(output_path, fourcc, fps, (width, height))
 
     while capture.isOpened():
         ret, frame = capture.read()
-        if ret == False:
+        if not ret:
             break
         
-        annotated_frame = detect_obj(frame)
+        annotated_frame, counts = detect_obj(frame, conf)
+
         out.write(annotated_frame)
-        annotated_frame = resize_frame(annotated_frame)
-        cv.imshow("vid", annotated_frame)
+        cv.imshow("per frame", resize_frame(annotated_frame, scale))
 
         if cv.waitKey(1) & 0xFF==ord('q'):
             break
 
     capture.release()
+    out.release()
+
+
+def track_objects(video_path, conf=0.6, scale=1):
+    model = YOLO("models/yolov8m.pt")
+
+    capture = cv.VideoCapture(video_path)
+
+    width = int(capture.get(cv.CAP_PROP_FRAME_WIDTH))
+    height = int(capture.get(cv.CAP_PROP_FRAME_HEIGHT))
+    fps = int(capture.get(cv.CAP_PROP_FPS))
+
+    basename = os.path.splitext(os.path.basename(video_path))[0]
+    output_path = f"output/{basename}_{conf}_track_out.mp4"
+    fourcc = cv.VideoWriter_fourcc(*"mp4v")
+
+    out = cv.VideoWriter(output_path, fourcc, fps, (width, height))
+
+    while capture.isOpened():
+        ret, frame = capture.read()
+        if not ret:
+            break
+
+        results = model.track(frame, conf=conf, persist=True)
+        annotated_frame = results[0].plot()
+
+        out.write(annotated_frame)
+        cv.imshow("tracking", resize_frame(annotated_frame, scale))
+
+        if cv.waitKey(1) & 0xFF==ord('q'):
+            break
+
+    out.release()
+    capture.release()
+
 
 if __name__ == "__main__":
     main()
